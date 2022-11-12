504d2cb2bcfa52c475c6b5e7a773122fe674f5fa
Dmytro Tristan <dmytro.tristan@dxc.com>
2022-11-12 20:45:29 +0100

    Performed a general refactoring

---
+++ signal_handler.hpp
@@ -0,0 +1,56 @@
+#ifndef SIGNAL_HANDLER_HPP
+#define SIGNAL_HANDLER_HPP
+#include <iostream>
+#include <functional>
+#include <array>
+#include <vector>
+#include <memory>
+
+#include <csignal>
+
+namespace tristan {
+
+    class [[maybe_unused]] SignalHandler {
+        SignalHandler();
+
+    public:
+        SignalHandler(const SignalHandler&) = delete;
+        SignalHandler(SignalHandler&&) = delete;
+        SignalHandler& operator=(const SignalHandler&) = delete;
+        SignalHandler& operator=(SignalHandler&&) = delete;
+        ~SignalHandler() = default;
+
+        static void registerHandler(int signal, std::function< void() > handler);
+        template < class Object > [[maybe_unused]] static void registerHandler(int signal, std::weak_ptr<Object> object, void (Object::*functor)()){
+            SignalHandler::registerHandler(signal, [object, functor]{
+                if (auto l_object = object.lock()){
+                    std::invoke(functor, l_object);
+                }
+            });
+        }
+        template < class Object > [[maybe_unused]] static void registerHandler(int signal, Object* object, void (Object::*functor)()){
+            SignalHandler::registerHandler(signal, [object, functor] {
+                if (object != nullptr){
+                    std::invoke(functor, object);
+                }
+            });
+        }
+
+        [[maybe_unused]] static void raiseSignal(int signal, bool value = true);
+
+    protected:
+    private:
+        static SignalHandler& instance();
+        static void _handleSignals(int signal);
+
+#if defined __linux__
+        std::array< std::vector< std::function< void() > >, 31 > m_handlers;
+        std::array< bool, 31 > m_signalThatShouldBeRaised;
+        struct sigaction m_action;
+#endif
+#if defined __WIN32 || defined __WIN64
+#endif
+    };
+
+}  // namespace tristan
+#endif  // SIGNAL_HANDLER_HPP
